c02567a37253c295c7ef8c4b46b149fa1417f495
Adam McMaster <adam.mcmaster@open.ac.uk>
2020-10-16 16:17:05 +0100

    Start stubbing out Django project

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python:3.7
+
+RUN apt-get update \
+  && apt-get install -y --no-install-recommends \
+    netcat \
+    postgresql-client \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN pip install \
+  pipenv
+
+WORKDIR /usr/src/app
+
+COPY Pipfile ./
+COPY Pipfile.lock ./
+
+RUN pipenv install --system --dev
+
+COPY . .
+
+ENV PYTHONUNBUFFERED=1
+
+EXPOSE 8080
+
+CMD ["bash", "start_server.sh"]
---
+++ start_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo Applying migrations
+python manage.py migrate --noinput
+
+if [ "$DJANGO_ENV" == "production" ] || [ "$DJANGO_ENV" == "staging" ]; then
+  if [ ! -d "hamlet/static" ]; then
+    echo Generating static files
+    python manage.py collectstatic --clear --no-input
+  fi
+  echo Starting production server
+  exec gunicorn hamlet.wsgi -b 0:8080 --access-logfile - --capture-output
+else
+  echo Starting development server
+  exec python manage.py runserver 0:8080
+fi
