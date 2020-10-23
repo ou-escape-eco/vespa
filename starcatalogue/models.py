e92c063f3a6bb8f1048f0f8b0cf6ff3e30766651
Adam McMaster <adam.mcmaster@open.ac.uk>
2020-10-23 14:01:59 +0100

    Add fields to models

--- starcatalogue/models.py
+++ starcatalogue/models.py
@@ -6,9 +6,43 @@ class Star(models.Model):
 
 
 class FoldedLightcurve(models.Model):
+    PULSATOR = 1
+    EA_EB = 2
+    EW = 3
+    ROTATOR = 4
+    UNKNOWN = 5
+    JUNK = 6
+    CLASSIFICATION_CHOICES = [
+        (PULSATOR, 'Pulsator'),
+        (EA_EB, 'EA/EB'),
+        (EW, 'EW'),
+        (ROTATOR, 'Rotator'),
+        (UNKNOWN, 'Unknown'),
+        (JUNK, 'Junk'),
+    ]
+
+    CERTAIN = 0
+    UNCERTAIN = 1
+    PERIOD_UNCERTAINTY_CHOICES = [
+        (CERTAIN, 'Certain'),
+        (UNCERTAIN, 'Uncertain'),
+    ]
+
     star = models.ForeignKey(to=Star, on_delete=models.CASCADE)
+    
+    period_number = models.IntegerField()
+    period_length = models.FloatField()
+    sigma = models.FloatField()
+    chi_squared = models.FloatField()
+    classification = models.IntegerField(choices=CLASSIFICATION_CHOICES)
+    period_uncertainty = models.IntegerField(choices=PERIOD_UNCERTAINTY_CHOICES)
+    classification_count = models.IntegerField()
 
 
 class ZooniverseSubject(models.Model):
     zooniverse_id = models.IntegerField(unique=True)
     lightcurve = models.OneToOneField(to=FoldedLightcurve, on_delete=models.CASCADE)
+
+    subject_set_id = models.IntegerField()
+    retired_at = models.DateTimeField()
+    image_location = models.URLField()
