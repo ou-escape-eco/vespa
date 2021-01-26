8127dbec3e75d52cd846a1ef8dbf20722fab18c1
Adam McMaster <adam.mcmaster@open.ac.uk>
2021-01-26 16:03:25 +0000

    Export DJANGO_SETTINGS_MODULE in prod

--- start_server.sh
+++ start_server.sh
@@ -3,13 +3,14 @@
 echo Applying migrations
 python manage.py migrate --noinput
 
-if [ "$DJANGO_ENV" == "production" ] || [ "$DJANGO_ENV" == "staging" ]; then
+if [ "$DJANGO_ENV" == "production" ]; then
+  export DJANGO_SETTINGS_MODULE="vespa.settings_production"
   if [ ! -d "vespa/static" ]; then
     echo Generating static files
     python manage.py collectstatic --clear --no-input
   fi
   echo Starting production server
-  exec gunicorn vespa.wsgi -b 0:8080 --access-logfile - --capture-output --env DJANGO_SETTINGS_MODULE=vespa.settings_production
+  exec gunicorn vespa.wsgi -b 0:8080 --access-logfile - --capture-output
 else
   echo Starting development server
   exec python manage.py runserver 0:8080
