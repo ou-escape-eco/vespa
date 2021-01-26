6169481fec780da0965003f5dee2ca7f6556d16f
Adam McMaster <adam.mcmaster@open.ac.uk>
2021-01-26 14:49:07 +0000

    Add gunicorn for prod deployment

--- start_server.sh
+++ start_server.sh
@@ -4,12 +4,12 @@ echo Applying migrations
 python manage.py migrate --noinput
 
 if [ "$DJANGO_ENV" == "production" ] || [ "$DJANGO_ENV" == "staging" ]; then
-  if [ ! -d "hamlet/static" ]; then
+  if [ ! -d "vespa/static" ]; then
     echo Generating static files
     python manage.py collectstatic --clear --no-input
   fi
   echo Starting production server
-  exec gunicorn hamlet.wsgi -b 0:8080 --access-logfile - --capture-output
+  exec gunicorn vespa.wsgi -b 0:8080 --access-logfile - --capture-output
 else
   echo Starting development server
   exec python manage.py runserver 0:8080
