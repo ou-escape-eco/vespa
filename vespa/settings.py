7eadb48b430e0e7f5b3382067420cadfa562d01e
Adam McMaster <adam.mcmaster@open.ac.uk>
2021-03-04 15:52:02 +0000

    Switch to postgres for local dev

--- vespa/settings.py
+++ vespa/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'vespa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db',
+        'NAME': 'vespa',
+        'USER': 'vespa',
+        'PASSWORD': 'localdev',
     }
 }
 
